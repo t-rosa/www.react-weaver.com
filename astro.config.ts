e52d883ff887f224a714a324dcc82c5790e2f8b6
Toma Rosa <toma.rosa@icloud.com>
2025-11-27 21:30:04 +0100

    chore: update astro configuration to include experimental font support and compression
    Signed-off-by: Toma Rosa <toma.rosa@icloud.com>

--- astro.config.ts
+++ astro.config.ts
@@ -1,11 +1,32 @@
 // @ts-check
-import { defineConfig } from "astro/config";
-
 import tailwindcss from "@tailwindcss/vite";
+import { defineConfig, fontProviders } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://www.react-weaver.com",
+  compressHTML: true,
   vite: {
     plugins: [tailwindcss()],
+  },
+  experimental: {
+    svgo: true,
+    fonts: [
+      {
+        provider: fontProviders.fontsource(),
+        name: "Montserrat",
+        cssVariable: "--font-montserrat",
+      },
+      {
+        provider: fontProviders.fontsource(),
+        name: "Inter",
+        cssVariable: "--font-inter",
+      },
+      {
+        provider: fontProviders.fontsource(),
+        name: "IBM Plex Mono",
+        cssVariable: "--font-ibm-plex-mono",
+      },
+    ],
   },
 });
